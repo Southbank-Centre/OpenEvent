eba9318fa9f56d007c86dc2091ce8d2cbad01c5c
Jorge Lopez-Lago <jorgelopezlago@users.noreply.github.com>
2015-04-27 16:00:52 +0100

    Update components-spec.js
    Added tests for:
    - create new content type
    - associate fields to content type created
    - create node of content type, associate Heading paragraph and test

--- test/functional/components/components-spec.js
+++ test/functional/components/components-spec.js
@@ -3,7 +3,17 @@
  * OE Components Spec
  */
 
+var url = require('url');
+var path = require('path');
+
 describe('OE Components', function() {
+  var contentTypeName = 'Component test';
+  var contentTypeMachineName = 'component_test'; // Lowercase and underscore
+  var contentTypePathName = 'component-test'; // Lowercase and hyphened
+
+  // Expected conditions
+  // @see http://angular.github.io/protractor/#/api?view=ExpectedConditions
+  var EC = protractor.ExpectedConditions;
 
   beforeEach(function(){
     isAngularSite(false);
@@ -50,5 +60,131 @@ describe('OE Components', function() {
   it('includes a Storify embed component', function(){
     expect(element(by.xpath("//table/tbody/tr[td='Storify embed (storify_embed)']")).isPresent()).toBe(true);
   });
+
+  it('creates a new content type', function(){
+    // Creates content type
+    browser.get(browser.params.url + '/admin/structure/types/add');
+    // Add name
+    element(by.id('edit-name')).sendKeys(contentTypeName);
+    // Disable revisions (this assumes diff module is enabled)
+    element(by.cssContainingText('ul.vertical-tabs-list > li > a', 'Compare revisions')).click();
+    element(by.id('edit-diff-enable-revisions-page-node')).isSelected().then(function(selected) {
+      if (selected) {
+          element(by.id('edit-diff-enable-revisions-page-node')).click();
+      }
+    });
+    // All other settings as default
+    // Save content type
+    element(by.id('edit-submit')).click();
+
+    // Check that the content type is listed (rather than looking for success message from Drupal)
+    browser.get(browser.params.url + '/admin/structure/types');
+    expect(element(by.id('content')).getText()).toContain(contentTypeName);
+  });
+
+  it('adds components fields to content type', function() {
+    // Enable Field UI module
+    browser.get(browser.params.url + '/admin/modules');
+    global.fieldUiStatus = element(by.id('edit-modules-core-field-ui-enable')).isSelected();
+    element(by.id('edit-modules-core-field-ui-enable')).isSelected().then(function(selected) {
+      if (!selected) {
+        element(by.id('edit-modules-core-field-ui-enable')).click();
+      }
+    });
+    element(by.id('edit-submit')).click();
+
+    // Add components field
+    browser.get(browser.params.url + '/admin/structure/types/manage/' + contentTypePathName + '/fields');
+    // Add new field
+    element(by.id('edit-fields-add-existing-field-label')).sendKeys('Components');
+    element(by.cssContainingText('#edit-fields-add-existing-field-field-name > option', 'Paragraphs: field_components (Components)')).click();
+    element(by.cssContainingText('#edit-fields-add-existing-field-widget-type > option', 'Embedded')).click();
+    element(by.id('edit-submit')).click();
+    // Configure field
+    browser.get(browser.params.url + '/admin/structure/types/manage/' + contentTypePathName + '/fields/field_components');
+    // Todo: select all checkboxes automatically
+    element(by.id('edit-instance-settings-allowed-bundles-table-flickr-embed-enabled')).click();
+    element(by.id('edit-instance-settings-allowed-bundles-table-heading-enabled')).click();
+    element(by.id('edit-instance-settings-allowed-bundles-table-html-enabled')).click();
+    element(by.id('edit-instance-settings-allowed-bundles-table-image-enabled')).click();
+    element(by.id('edit-instance-settings-allowed-bundles-table-link-enabled')).click();
+    element(by.id('edit-instance-settings-allowed-bundles-table-long-text-enabled')).click();
+    element(by.id('edit-instance-settings-allowed-bundles-table-page-element-spec-enabled')).click();
+    element(by.id('edit-instance-settings-allowed-bundles-table-quote-enabled')).click();
+    element(by.id('edit-instance-settings-allowed-bundles-table-soundcloud-embed-enabled')).click();
+    element(by.id('edit-instance-settings-allowed-bundles-table-storify-embed-enabled')).click();
+    element(by.id('edit-instance-settings-allowed-bundles-table-youtube-embed-enabled')).click();
+    element(by.cssContainingText('#edit-instance-settings-add-mode > option', 'Select List')).click();
+    element(by.id('edit-submit')).click();
+  });
+
+  it('tests Heading component: creates a new node of the content type and add a paragraph to it', function() {
+    // Todo: Limit the element creation to editor role
+    browser.get(browser.params.url + '/node/add/' + contentTypePathName);
+    expect(dvr.findElement(by.css('.page-title')).getText()).toContain('Create ' + contentTypeName);
+
+    // Heading paragraph
+    element(by.cssContainingText('#edit-field-components-und-add-more-type > option', 'Heading')).click();
+    element(by.id('edit-field-components-und-add-more-add-more')).click();
+    browser.wait(function() {
+      return browser.isElementPresent(by.id('field-components-values'));
+    }, 5000);
+    expect(element(by.id('edit-field-components-und-0-paragraph-bundle-title')).getText()).toContain('Paragraph type: Heading');
+    browser.wait(function() {
+      return browser.isElementPresent(by.id('edit-field-components-und-0-field-heading-level-und'));
+    }, 5000);
+
+    // Submit without required fields
+    element(by.id('edit-submit')).click();
+    expect(element(by.css('.messages')).getText()).toContain('Title field is required.');
+    expect(element(by.css('.messages')).getText()).toContain('Heading field is required.');
+
+    // Fill in required fields
+    element(by.id('edit-title')).sendKeys('Test content');
+    element(by.css('#edit-field-components-und-0-field-heading-level-und > .form-item-field-components-und-0-field-heading-level-und:nth-of-type(4) > input')).click();
+    element(by.id('edit-field-components-und-0-field-heading-und-0-value')).sendKeys('This is a heading');
+    // Submit the node and check for successful creation message
+    element(by.id('edit-submit')).click();
+    expect(element(by.css('.messages')).getText()).toContain(contentTypeName + ' Test content has been created.');
+  });
+
+  //it('tests Flickr embed component: creates a new node of the content type and add a paragraph to it', function() {});
+
+  //it('tests HTML component: creates a new node of the content type and add a paragraph to it', function() {});
+
+  //it('tests Image component: creates a new node of the content type and add a paragraph to it', function() {});
+
+  //it('tests Link component: creates a new node of the content type and add a paragraph to it', function() {});
+
+  //it('tests Long text component: creates a new node of the content type and add a paragraph to it', function() {});
+
+  //it('tests Quote component: creates a new node of the content type and add a paragraph to it', function() {});
+
+  //it('tests SoundCloud embed component: creates a new node of the content type and add a paragraph to it', function() {});
+
+  //it('tests Storify embed component: creates a new node of the content type and add a paragraph to it', function() {});
+
+  //it('tests YouTube embed component: creates a new node of the content type and add a paragraph to it', function() {});
+
+  //it('deletes content created for the test', function() {});
+
+  //it('deletes the content type created for the test', function() {
+  // // Clean up actions (part of another test):
+  // - Disable Field UI module (use fieldUiStatus)
+  // - Delete content
+  // - Delete content type
+
+  //function CleanUp() {
+  //  describe('Clean up', function () {
+  //    it('can revert changes made in this spec', function () {
+  //      // Delete content (fields deleted automatically)
+  //      // <tbd>
+  //      // Delete content type
+  //      browser.get(browser.params.url + '/admin/structure/types/manage/contentTypePathName/delete');
+  //      element(by.id('edit-submit')).click();
+  //    });
+  //  });
+  //}
+  // });
 
 });
