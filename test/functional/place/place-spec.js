cc1b39b1009d338331e8188688903f58d36b6d44
samueljmurray <samueljmurray@gmail.com>
2015-05-06 18:27:34 +0100

    added more to place spec

--- test/functional/place/place-spec.js
+++ test/functional/place/place-spec.js
@@ -16,6 +16,7 @@ describe('The Place features of the CMS', function() {
   beforeEach(function(){
     // don't wait for (non-existent) Angular to load
     isAngularSite(false);
+    browser.manage().timeouts().implicitlyWait(5000);
   });
 
   it('can allow places to be viewed by anyone', function() {
@@ -42,11 +43,15 @@ describe('The Place features of the CMS', function() {
     dvr.findElement(by.id('edit-submit')).click();
 
     // check for the error message explaining that required fields haven't been populated
-    expect(dvr.findElement(by.id('console')).getText()).toContain('Title field is required');
+    expect(dvr.findElement(by.id('console')).getText()).toContain('Name field is required');
 
     // fill out content on 'Main' tab
     dvr.findElement(by.xpath("//ul[@class='vertical-tabs-list']/li/a[strong='Main']")).click();
     dvr.findElement(by.id('edit-title')).sendKeys('Parent place');
+
+    // set the item to published
+    dvr.findElement(by.xpath("//ul[@class='vertical-tabs-list']/li/a[strong='Publishing options']")).click();
+    dvr.findElement(by.id('edit-status')).click();
 
     // submit
     dvr.findElement(by.id('edit-submit')).click();
@@ -97,6 +102,9 @@ describe('The Place features of the CMS', function() {
     $('#edit-field-image-und-0-alt').sendKeys('Test image ALT');
     $('#edit-field-image-und-0-title').sendKeys('Test image TITLE');
 
+    // test that a second image could be added
+    expect(element(by.id('edit-field-image-und-1-upload')).isPresent()).toBe(true);
+
     // type in the title of the page created in the above test and wait for the autocomplete list to load
     dvr.findElement(by.css('#edit-field-place-parents tr:last-of-type input[type="text"]')).sendKeys('Parent place');
     dvr.wait(function () {
@@ -125,7 +133,7 @@ describe('The Place features of the CMS', function() {
 
       // add geolocation
       element(by.id('edit-field-place-geolocation-und-0-address-field')).sendKeys('Southbank Centre London');
-      element(by.id('geolocation-address-geocode-44-0')).click();
+      element(by.id('geolocation-address-geocode-49-0')).click();
 
       // wait for Google Maps
       browser.sleep(5000);
@@ -241,8 +249,14 @@ describe('The Place features of the CMS', function() {
     expect(element(by.xpath("//div[@id='autocomplete']//li[1]//span[@class='field-content']/a")).isPresent()).toBe(false);
     element(by.xpath("//div[@id='autocomplete']//li[1]//span[@class='field-content']")).click();
 
-    // submit
-    dvr.findElement(by.id('edit-submit')).click();
+    // re-focus
+    element(by.css('body')).click();
+    // save
+    element(by.id('edit-submit')).click();
+    dvr.wait(function() {
+      return element(by.cssContainingText('#console', 'Place Parent place has been updated.'));
+    }, 5000);
+    expect(element(by.id('console')).getText()).toContain('Place Parent place has been updated.');
 
   });
 
@@ -253,84 +267,81 @@ describe('The Place features of the CMS', function() {
       .expectStatus(200)
       .expectHeaderContains('content-type', 'application/json')
       .expectJSON({
-        "field_teaser": {
-          "value": "<p>Here is some content in the teaser field <em>that contains emphasis</em> but doesNotContainJavascript();</p>\n",
-          "format": "filtered_html"
-        },
         "field_description": {
           "value": "<p>Here is some content in the description field <em>that contains emphasis</em> but doesNotContainJavascript();</p>\n",
           "format": "filtered_html"
         },
-        "field_event_age_range": "4+",
-        "field_event_class": {
-          "uri": function(val) { expect(val).toContain(browser.params.url + "/taxonomy_term/"); },
-          "id": function(val) {
-            expect(val).toBeDefined();
-            expect(isNaN(parseInt(val, 10))).toBe(false);
-          },
-          "resource": "taxonomy_term"
-        },
-        "field_event_type": [
+        "field_image": [
           {
-            "uri": function(val) { expect(val).toContain(browser.params.url + "/taxonomy_term/"); },
-            "id": function(val) {
-              expect(val).toBeDefined();
-              expect(isNaN(parseInt(val, 10))).toBe(false);
+            "file": {
+              "uri": function(val) {
+                expect(val).toContain(browser.params.url + "/file/");
+              },
+              "id": function(val) {
+                expect(val).toBeDefined();
+                expect(isNaN(parseInt(val, 10))).toBe(false);
+              },
+              "resource": "file"
             },
-            "resource": "taxonomy_term"
-          },
-          {
-            "uri": function(val) { expect(val).toContain(browser.params.url + "/taxonomy_term/"); },
-            "id": function(val) {
-              expect(val).toBeDefined();
-              expect(isNaN(parseInt(val, 10))).toBe(false);
-            },
-            "resource": "taxonomy_term"
+            "alt": "Test image ALT",
+            "title": "Test image TITLE"
           }
         ],
-        "field_event_parents": [
+        "field_place_parents": [
           {
             "uri": browser.params.url + "/node/" + parentNid,
             "id": parentNid,
             "resource": "node"
           }
         ],
-        "field_event_children": [],
-        "field_event_date_time": {
-          "value": function(val) { 
-            expect(val.length).toEqual(10); 
-            expect(isNaN(parseInt(val, 10))).toBe(false);
-          },
-          "value2": function(val) { 
-            expect(val.length).toEqual(10); 
-            expect(isNaN(parseInt(val, 10))).toBe(false);
-          },
-          "duration": function(val) { expect(typeof val).toEqual("number"); },
+        "field_place_address": {
+          "country": 'GB',
+          "administrative_area": "London (county)",
+          "locality": "London",
+          "postal_code": "SE1 8XX",
+          "thoroughfare": "Southbank Centre",
+          "premise": "Belvedere Road",
         },
-        "field_event_door_time": function(val) {
-          expect(val.length).toEqual(10);
-          expect(isNaN(parseInt(val, 10))).toBe(false);
-        },
-        "field_event_duration": "150",
-        "cer": {
-          "lineage": "node:event:",
-          "depth": 0,
-          "owner": {
-            "uri": browser.params.url + "/node/" + nid,
-            "id": nid,
-            "resource": "node"
+        "field_opening_hours": [
+          {
+            "day": "1",
+            "starthours": "700",
+            "endhours": "1500"
           },
-          "original": {
-            "uri": browser.params.url + "/node/" + nid,
-            "id": nid,
-            "resource": "node"
+          {
+            "day": "2",
+            "starthours": "800",
+            "endhours": "1600"
+          },
+          {
+            "day": "3",
+            "starthours": "900",
+            "endhours": "1700"
+          },
+          {
+            "day": "4",
+            "starthours": "1000",
+            "endhours": "1800"
+          },
+          {
+            "day": "5",
+            "starthours": "1100",
+            "endhours": "200"
+          },
+        ],
+        "field_place_geolocation": {
+          "lat": function(val) {
+            expect(isNaN(parseFloat(val))).toBe(false); 
+          },
+          "lng": function(val) {
+            expect(isNaN(parseFloat(val))).toBe(false); 
           }
         },
         "nid": nid,
         "vid": nid,
         "is_new": function(val) { expect(typeof val).toEqual("boolean"); },
-        "type": "event",
-        "title": "Protractor event page",
+        "type": "place",
+        "title": "Protractor place",
         "language": "und",
         "url": browser.params.url + '/' + pathAlias,
         "edit_url": browser.params.url + "/node/" + nid + "/edit",
@@ -351,23 +362,7 @@ describe('The Place features of the CMS', function() {
           "format": null
         }
       })
-      .after(function() {
-
-        frisby.create('Get JSON for Event page created in previous test')
-          .get(browser.params.url + '/node.json?nid=' + parentNid)
-          .expectStatus(200)
-          .expectHeaderContains('content-type', 'application/json')
-          .expectJSON('list.0.field_event_children', [
-            {
-              "uri": browser.params.url + "/node/" + nid,
-              "id": nid,
-              "resource": "node"
-            }
-          ])
-          .after(CleanUp)
-          .toss();
-
-      })
+      .after(CleanUp)
       .toss();
 
     function CleanUp() {
@@ -377,32 +372,6 @@ describe('The Place features of the CMS', function() {
         it('will take place after all tests have passed', function() {
 
           // CLEAN UP
-          // remove event class terms
-          browser.get(browser.params.url + '/admin/structure/taxonomy/event_class');
-          dvr.findElement(by.css('#taxonomy tr:first-of-type td:nth-of-type(3) a')).click();
-          dvr.findElement(by.id('edit-delete')).click();
-          dvr.findElement(by.id('edit-submit')).click();
-          dvr.findElement(by.css('#taxonomy tr:first-of-type td:nth-of-type(3) a')).click();
-          dvr.findElement(by.id('edit-delete')).click();
-          dvr.findElement(by.id('edit-submit')).click();
-          dvr.findElement(by.css('#taxonomy tr:first-of-type td:nth-of-type(3) a')).click();
-          dvr.findElement(by.id('edit-delete')).click();
-          dvr.findElement(by.id('edit-submit')).click();
-          expect(dvr.findElement(by.css('#taxonomy tr:first-of-type td:nth-of-type(1)')).getText()).toContain('No terms available.');
-
-          // remove event type terms
-          browser.get(browser.params.url + '/admin/structure/taxonomy/event_type');
-          dvr.findElement(by.css('#taxonomy tr:first-of-type td:nth-of-type(3) a')).click();
-          dvr.findElement(by.id('edit-delete')).click();
-          dvr.findElement(by.id('edit-submit')).click();
-          dvr.findElement(by.css('#taxonomy tr:first-of-type td:nth-of-type(3) a')).click();
-          dvr.findElement(by.id('edit-delete')).click();
-          dvr.findElement(by.id('edit-submit')).click();
-          dvr.findElement(by.css('#taxonomy tr:first-of-type td:nth-of-type(3) a')).click();
-          dvr.findElement(by.id('edit-delete')).click();
-          dvr.findElement(by.id('edit-submit')).click();
-          expect(dvr.findElement(by.css('#taxonomy tr:first-of-type td:nth-of-type(1)')).getText()).toContain('No terms available.');
-
           // remove content
           browser.get(browser.params.url + '/admin/content');
           dvr.findElement(by.css('#node-admin-content > div > table:nth-of-type(2) > thead:first-of-type > tr:first-of-type > th:first-of-type input')).click();
